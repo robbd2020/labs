72ff349aabed1d9fba64b6be7ad9b159c1021552
Rob <r.h.schekman@gmail.com>
2020-09-17 14:34:21 +0200

    Refactor Banking tests

--- src/test/java/h7/banking/BankTest.java
+++ src/test/java/h7/banking/BankTest.java
@@ -4,19 +4,22 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class BankTest {
 
     Bank abn;
+    BankAccount piet;
+    BankAccount sarah;
+    BankAccount guus;
+    BankAccount moniek;
 
     @Before
     public void init() {
         abn = new Bank();
-        BankAccount piet = abn.openNewAccount();
-        BankAccount sarah = abn.openNewAccount();
-        BankAccount guus = abn.openNewAccount();
-        BankAccount moniek = abn.openNewAccount();
+        piet = abn.openNewAccount();
+        sarah = abn.openNewAccount();
+        guus = abn.openNewAccount();
+        moniek = abn.openNewAccount();
 
         piet.deposit(33.33);
         sarah.deposit(999.21);
@@ -30,25 +33,27 @@ public class BankTest {
 
     @Test
     public void whenMoreMoneyThanBalanceIsWithdrawn() {
-        assertEquals(0d, abn.getBankAccountByNumber(1).performWithdraw(35), .1);
+        assertEquals(0d, piet.performWithdraw(35), .1);
     }
 
     @Test
-    public void whenMoneyIsTransferredItIsWithdrawnCorrectly(){
-        abn.transferMoney(2,1,100d);
-        assertEquals(abn.getBankAccountByNumber(1).getBalance(), 133.33, 0.1);
+    public void whenMoneyIsTransferredItIsWithdrawnCorrectly() {
+        abn.transferMoney(2, 1, 100d);
+        assertEquals(piet.getBalance(), 133.33, 0.1);
     }
 
     @Test
-    public void whenMoneyIsTransferredItIsDepositedCorrectly(){
-        abn.transferMoney(2,1,100d);
-        assertEquals(abn.getBankAccountByNumber(2).getBalance(), 899.21, 0.1);
+    public void whenMoneyIsTransferredItIsDepositedCorrectly() {
+        abn.transferMoney(2, 1, 100d);
+        assertEquals(sarah.getBalance(), 899.21, 0.1);
     }
 
     @Test
-    public void whenMoreThanAvailableMoneyIsTransferredNothingWithdrawnOrDeposited(){
-        abn.transferMoney(2,3,1000d);
-        assertEquals(1_000_000,abn.getBankAccountByNumber(3).getBalance(),.1);
-        assertEquals(999.21,abn.getBankAccountByNumber(2).getBalance(),.1);
+    public void whenMoreThanAvailableMoneyIsTransferredNothingWithdrawnOrDeposited() {
+        abn.transferMoney(2, 3, 1000d);
+        assertEquals(1_000_000, guus.getBalance(), .1);
+        assertEquals(999.21, sarah.getBalance(), .1);
+        assertEquals(0, moniek.getBalance(), .1);
+        assertEquals(33.33, piet.getBalance(), .1);
     }
 }
