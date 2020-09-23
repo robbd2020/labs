7c930779bd7739952f9f68d19580ae494e56b71a
Rob <r.h.schekman@gmail.com>
2020-09-23 19:20:59 +0200

    Change StringAdder to use stream

--- src/main/java/unittesting/StringAdder.java
+++ src/main/java/unittesting/StringAdder.java
@@ -1,28 +1,28 @@
 package unittesting;
 
+import java.util.Arrays;
+
 public class StringAdder {
 
     public static final int ZERO = 0;
 
     public int add(String s) {
-        int number = ZERO;
+
         String delimiter = ",";
 
 //        try {
 
-            if (s.length() >= 2 && s.startsWith("//")) {
-                delimiter = Character.toString(s.charAt(2));
-                s = s.substring(2);
-            }
+        if (s.length() >= 2 && s.startsWith("//")) {
+            delimiter = Character.toString(s.charAt(2));
+            s = s.substring(2);
+        }
 
-            String[] splitted = s.replace(" ", "").split("[" + delimiter + "|\n]");
+        return Arrays.stream(s.replace(" ", "")
+                .split("[" + delimiter + "|\n]"))
+                .filter(c -> !c.isEmpty())
+                .mapToInt(Integer::parseInt)
+                .reduce(0, Integer::sum);
 
-            for (String s1 : splitted) {
-                if (!s1.isEmpty())
-                    number += Integer.parseInt(s1);
-            }
-
-            return number;
 //        } catch (Exception e) {
 //            throw new UnsupportedOperationException("Not yet implemented");
 //        }
